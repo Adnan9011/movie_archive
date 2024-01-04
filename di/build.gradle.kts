70b25d81798857289da79e2eb0b66babfced0f2c
Adnan9011 <adnan9011@gmail.com>
2024-01-04 18:23:05 +0330

    feat : Add di module

---
+++ di/build.gradle.kts
@@ -0,0 +1,52 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.moviearchive.di"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    projects.apply {
+        implementation(navigation)
+        implementation(feature)
+        implementation(domain)
+        implementation(data)
+    }
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+
+    implementation(libs.koin.android)
+    implementation(libs.koin.test.junit4)
+    implementation(libs.koin.ksp.annotation)
+    implementation(libs.koin.ksp)
+}
